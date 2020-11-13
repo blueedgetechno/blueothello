7a7eb81c03b8d8e7f32e862afeec40de696967a4
blueedgetechno <blueedgetechno@gmail.com>
2020-11-14 01:18:20 +0530

    othello

---
+++ README.md
@@ -0,0 +1,7 @@
+# Othello , also Reversi
+
+### Othello in Atom !
+
+keybinding : ctrl+alt+h
+
+![view](https://raw.githubusercontent.com/blueedgetechno/othello/master/img/view.png)
---
+++ spec/blueothello-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BlueothelloView from '../lib/blueothello-view';
+
+describe('BlueothelloView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
